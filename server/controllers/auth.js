8c835d8378a9f9390cf48ce6da705e9bd613b485
Azzam Zafar <azzam.zafar21@gmail.com>
2023-06-11 17:07:04 +0530

    feat:passwd encrypt. using argon2

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,8 @@
-const User = require('../models/users.js');
 const UserModel = require('../models/users.js');
 const sequelize = require('../utils/db_con.js');
-
+const argon2 = require('argon2')
 exports.postAddUser = async (req,res,next) =>{
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+    const {username,email,password} = req.body;
     try{
         const result = await UserModel.findAll({
             where:{email:email}
@@ -15,11 +12,13 @@ exports.postAddUser = async (req,res,next) =>{
             res.status(409).json({msg:"User Already Exists"})
         }
         else{
+            const hash = await argon2.hash(password,{type:argon2.argon2id});
+
             const result = await sequelize.transaction(async (t)=>{
                 return await UserModel.create({
                     username:username,
                     email:email,
-                    password:password
+                    password:hash
                 },{transaction:t})
             })
             res.status(201).json({msg:"User Added"})
@@ -30,20 +29,19 @@ exports.postAddUser = async (req,res,next) =>{
 }
 
 exports.postLoginUser = async (req,res,next)=>{
-    const handle = req.body.email;
-    const password = req.body.password;
-    if (handle.includes('@')){
+    const {email,password} = req.body;
+    if (email.includes('@')){
         try{
             const users = await sequelize.transaction(async (t)=>{
                 return await UserModel.findAll({
-                    where:{email:handle}
+                    where:{email:email}
                 },{transaction:t})
             })
             const jusers = JSON.parse(JSON.stringify(users))
 
             if (jusers.length!=1) res.status(404).send("Not Found")
 
-            else if (jusers[0].password!=password) res.status(401).send("Password Incorrect")
+            else if (!(await argon2.verify(jusers[0].password,password))) res.status(401).send("Password Incorrect")
 
             else res.status(303).json({msg:"login successful"})
         }catch(err){
